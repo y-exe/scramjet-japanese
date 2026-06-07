837e71bf2ac67be65c41664c613280174c274838
y-exe <y.exe.1201@proton.me>
2026-06-08 00:00:04 +0900

    fix: Cloudflare デプロイビルドを修正

--- packages/core/rewriter/wasm/build.sh
+++ packages/core/rewriter/wasm/build.sh
@@ -22,7 +22,13 @@ if [ -f out/.build-hash ] && [ -f ../../dist/scramjet.wasm ] && [ "$SRC_HASH" !=
   exit 0
 fi
 
-which cargo wasm-bindgen wasm-opt wasm-snip &> /dev/null || {
+REQUIRED_TOOLS="cargo wasm-bindgen wasm-opt"
+if [ "${SKIP_WASM_SNIP:-0}" != "1" ]; then
+	REQUIRED_TOOLS="${REQUIRED_TOOLS} wasm-snip"
+fi
+
+# shellcheck disable=SC2086
+which $REQUIRED_TOOLS &> /dev/null || {
 	echo "Please install cargo, wasm-bindgen, wasm-opt from https://github.com/WebAssembly/binaryen, and wasm-snip from https://github.com/r58playz/wasm-snip!"
 	exit 1
 }
@@ -56,39 +62,43 @@ fi
 
 cd ../../
 
-wasm-snip rewriter/wasm/out/wasm_bg.wasm -o rewriter/wasm/out/wasm_snipped.wasm \
-	-p 'oxc_regular_expression::.*' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_non_array_type' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_import_type' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_operator_or_higher' \
-	'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_interface_declaration' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_mapped_type' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_index_signature_declaration' \
-	'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_import_equals_declaration' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_or_type_predicate' \
-	'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_namespace_or_module_declaration_body' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_implements_clause' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_intersection_type_or_higher' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_type_name' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_literal_type_node' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_asserts_type_predicate' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_tuple_element_type' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_arguments_of_type_reference' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_call_signature_member' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::is_start_of_type' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_this_type_predicate' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_query' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_reference' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_operator' \
-	'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_literal' \
-	'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::is_at_enum_declaration' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_element' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_identifier' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_element_name' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_children' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_fragment' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_expression_container' \
-	'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_expression'
+if [ "${SKIP_WASM_SNIP:-0}" = "1" ]; then
+	cp rewriter/wasm/out/wasm_bg.wasm rewriter/wasm/out/wasm_snipped.wasm
+else
+	wasm-snip rewriter/wasm/out/wasm_bg.wasm -o rewriter/wasm/out/wasm_snipped.wasm \
+		-p 'oxc_regular_expression::.*' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_non_array_type' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_import_type' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_operator_or_higher' \
+		'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_interface_declaration' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_mapped_type' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_index_signature_declaration' \
+		'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_import_equals_declaration' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_or_type_predicate' \
+		'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_ts_namespace_or_module_declaration_body' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_implements_clause' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_intersection_type_or_higher' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_type_name' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_literal_type_node' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_asserts_type_predicate' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_tuple_element_type' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_arguments_of_type_reference' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_ts_call_signature_member' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::is_start_of_type' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_this_type_predicate' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_query' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_reference' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_operator' \
+		'oxc_parser::ts::types::<impl oxc_parser::ParserImpl>::parse_type_literal' \
+		'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::is_at_enum_declaration' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_element' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_identifier' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_element_name' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_children' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_fragment' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_expression_container' \
+		'oxc_parser::jsx::<impl oxc_parser::ParserImpl>::parse_jsx_expression'
+fi
 #
 #	these are confirmed to break oxc
 #   'oxc_parser::ts::statement::<impl oxc_parser::ParserImpl>::parse_declaration' \
